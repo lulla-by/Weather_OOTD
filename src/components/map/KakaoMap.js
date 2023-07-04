cec8ffa15312e8814abd1fcd1cfdd614d081dda4
lulla-by <thfl3363@hanmail.net>
2023-07-04 20:34:47 +0900

    [Feat] 카카오 지도 및 현재 위치 기능 추가 - 카카오 지도 api 사용 - 현재 위치 설정

--- src/components/map/KakaoMap.js
+++ src/components/map/KakaoMap.js
@@ -1,8 +1,33 @@
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+const { kakao } = window;
+
 const KakaoMap = () => {
+  const data = useSelector(state => state.current)
+
+
+  async function onGeoOk(position) {
+    const WEATHER_KEY = process.env.REACT_APP_WEATHER_KEY
+    const coords = position.coords;
+    const { latitude, longitude } = coords;
+    const url = ` https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_KEY}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const container = document.getElementById("map")
+    const options = {
+      center: new kakao.maps.LatLng(data.coord.lat, data.coord.lon),
+      level: 5
+    }
+    const map = new kakao.maps.Map(container, options)
+  }
+  function onGeoError() {
+    console.log("Can't find you");
+  }
+  const position = navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
   return (
-    <div id="map" style={{width:"100%",height:"300px",backgroundColor:"skyblue"}}></div>
+    <div id="map" style={{ width: "100%", height: "300px", backgroundColor: "skyblue" }}></div>
   )
 }
 
