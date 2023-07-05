3fbbec8f53dff857a86d2bf0cb6eb08c5102ac3d
lulla-by <thfl3363@hanmail.net>
2023-07-05 14:56:26 +0900

    [Add] 현위치 초단기예보 조회 GET 요청 - 초단기예보 상세기능 조회로 endpoint 수정 - 현위치 위도 경도 좌표 변환 추가

--- src/components/contents/Contents.js
+++ src/components/contents/Contents.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Information from './Information';
 import Clothes from './Clothes';
+import { useSelector } from 'react-redux';
+import { dfsXyConv } from '../../xyConverter';
+import axios from 'axios';
 
 const Contents = () => {
+
+  const [nowWeather, setNowWether] = useState([])
+
+  const serviceKey = process.env.REACT_APP_WEATHER_KEY;
+  const numOfRows = 1000;
+  const pageNo = 1;
+  const base_date = '20230705';
+  const base_time = '0030';
+
+  const {lat,long} = useSelector(state => state.current)
+  const {x:nx,y:ny} = dfsXyConv("toXY",lat,long);
+
+
+
+  useEffect(() => {
+    if(lat === ""){
+      return ;
+    }else{
+      const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst` +
+        `?serviceKey=${serviceKey}` +
+        `&numOfRows=${numOfRows}&pageNo=${pageNo}` +
+        `&dataType=json&base_date=${base_date}&base_time=${base_time}&nx=${nx}&ny=${ny}`;
+  
+      axios.get(url)
+        .then(response => {
+          const data =response.data.response.body.items.item
+          console.log(data);
+
+        })
+        .catch(error => {
+          // 오류 처리
+          console.error(error);
+        });
+    }
+  }, [lat])
+
   return (
     <div>
-      <Information/>
-      <Clothes/>
+      <Information />
+      <Clothes />
     </div>
   )
 }
