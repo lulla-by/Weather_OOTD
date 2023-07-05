ce70878edc4f507532e5ebb196a9163a3c7b0ee7
lulla-by <thfl3363@hanmail.net>
2023-07-05 16:47:21 +0900

    [Add] 날짜 및 현재시각 기준 baseDate 및 baseTime 생성 - endpoint에 보내야하는 형식으로 변환시키는 dateTimeConverter()생성

--- src/components/contents/Contents.js
+++ src/components/contents/Contents.js
@@ -9,38 +9,56 @@ const Contents = () => {
 
   const [nowWeather, setNowWether] = useState([])
 
+  const dateTimeConverter = (a)=>{
+    if(a < 10){
+      return "0"+a.toString()
+    }else{
+      return a.toString()
+    }
+  }
+
+  let date = new Date();
+  
+  let year = date.getFullYear().toString()
+  let month = dateTimeConverter(date.getMonth()+1)
+  let day = dateTimeConverter(date.getDate())
+
+  let hours = dateTimeConverter(date.getHours())
+  let minutes = dateTimeConverter(date.getMinutes())
+
+
+  let baseDate = (year+month+day)
+  let baseTime = (hours+minutes > hours+"30" ? hours+"30" :hours-1+"30")
   const serviceKey = process.env.REACT_APP_WEATHER_KEY;
   const numOfRows = 1000;
   const pageNo = 1;
-  const base_date = '20230705';
-  const base_time = '0030';
 
-  const {lat,long} = useSelector(state => state.current)
-  const {x:nx,y:ny} = dfsXyConv("toXY",lat,long);
+
+  const { lat, long } = useSelector(state => state.current)
+  const { x: nx, y: ny } = dfsXyConv("toXY", lat, long);
 
 
 
   useEffect(() => {
-    if(lat === ""){
-      return ;
-    }else{
+    if (lat === "") {
+      return;
+    } else {
       const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst` +
         `?serviceKey=${serviceKey}` +
         `&numOfRows=${numOfRows}&pageNo=${pageNo}` +
-        `&dataType=json&base_date=${base_date}&base_time=${base_time}&nx=${nx}&ny=${ny}`;
-  
+        `&dataType=json&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
+
       axios.get(url)
         .then(response => {
-          const data =response.data.response.body.items.item
+          const data = response.data.response.body.items.item
           console.log(data);
-
         })
         .catch(error => {
           // 오류 처리
           console.error(error);
         });
     }
-  }, [lat])
+  }, [lat,date])
 
   return (
     <div>
