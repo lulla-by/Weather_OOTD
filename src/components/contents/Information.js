9fa90e63e4cb62682f6e52cb44ca5425e7e83c95
lulla-by <thfl3363@hanmail.net>
2023-07-06 21:07:32 +0900

    [Rename] getNowWeather로 함수 이름 변경 및 utils 디렉토리로 이동

--- src/components/contents/Information.js
+++ src/components/contents/Information.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import Card from '../../ui/Card'
 import classes from './information.module.css'
+import { getNowWeather } from '../../utils/getNowWeather';
 
 const Information = ({ props }) => {
 
   const { temperature, precipitation, humidity, precipitationType, skyCondition } = props;
 
-  const makeWeatherClassName = (precipitationType, skyCondition) => {
-    if (precipitationType === "없음") {
-      if (skyCondition === "구름 많음") {
-        return ["cloudy","구름 많음"];
-      } else if (skyCondition === "맑음") {
-        return ["sunny","맑음"];
-      } else if (skyCondition === "흐림") {
-        return[ "gloomy","흐림"];
-      }
-    } else if (precipitationType === "눈" || precipitationType === "비/눈") {
-      return ["snow","눈"];
-    } else if (precipitationType === "비") {
-      return ["rain","비"];
-    }
-  };
-
-  const weatherClasseName = makeWeatherClassName(precipitationType, skyCondition)
+  const weatherClasseName = getNowWeather(precipitationType, skyCondition)
   const weatherMsg = "현재 날씨: "+(weatherClasseName&&weatherClasseName[1])
 
   return (
