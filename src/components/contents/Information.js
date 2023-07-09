f8e20ed4cd7e44c50cce5b18a5dc2742cd53d257
lulla-by <thfl3363@hanmail.net>
2023-07-10 01:56:57 +0900

    [BugFix] Information, Clothes 컴포넌트 - 이미지 src 절대경로로 변경

--- src/components/contents/Information.js
+++ src/components/contents/Information.js
@@ -9,7 +9,7 @@ const Information = ({ props,data }) => {
   const weatherClasseName = getNowWeather(precipitationType, skyCondition)
   const weatherMsg = "현재 날씨: " + (weatherClasseName && weatherClasseName[1])
 
-  const weatherImgSrc = require(`../../assets/${weatherClasseName[1].split(" ").join("")}.png`)
+  const weatherImgSrc = require(`/src/assets/${weatherClasseName[1].split(" ").join("")}.png`)
 
 
   return (
